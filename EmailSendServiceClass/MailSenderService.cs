361bc7a6d8b3b8d446def92f830f2db2198ce7ae
Константин Харитонов <sdw200t@hotmail.com>
2020-10-01 12:20:40 +0500

    Создал библиотеку EmailSendServiceClass и подключил ее в проект

---
+++ EmailSendServiceClass/MailSenderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailSendServiceClass
+{
+    class MailSenderService
+    {
+        public void SendEmail(MailMessage Message, string SmtpAdress, int SmtpPort, string Login, string Password)
+        {
+            SmtpClient smtp = new SmtpClient(SmtpAdress, SmtpPort);
+            smtp.Credentials = new NetworkCredential(Login, Password);
+            smtp.EnableSsl = true;
+            smtp.Send(Message);
+        }
+    }
+}
